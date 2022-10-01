f1128ca5641a2af56bffb19972194533d92ba8ae
Monogato <monogato.dg@gmail.com>
2022-09-30 20:21:46 -0500

    Fix slint error useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ export default function Header() {
               <div className="flex items-center justify-between h-16">
                 <div className="flex items-center">
                   <div className="flex-shrink-0">
-                    <Image className="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+                    <Image className="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" width={32} height={32} />
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-10 flex items-baseline space-x-4">
@@ -69,7 +69,7 @@ export default function Header() {
                       <div>
                         <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                           <span className="sr-only">Open user menu</span>
-                          <Image className="h-8 w-8 rounded-full object-cover" src={userData.imageUrl} alt="" />
+                          <Image className="h-8 w-8 rounded-full object-cover" src={userData.imageUrl} alt="" width={32} height={32} />
                         </Menu.Button>
                       </div>
                       <Transition
@@ -117,7 +117,7 @@ export default function Header() {
               <div className="pt-4 pb-3 border-t border-gray-700">
                 <div className="flex items-center px-5">
                   <div className="flex-shrink-0">
-                    <Image className="h-10 w-10 rounded-full" src={userData.imageUrl} alt="" />
+                    <Image className="h-10 w-10 rounded-full" src={userData.imageUrl} alt="" width={40} height={40} />
                   </div>
                   <div className="ml-3">
                     <div className="text-base font-medium leading-none text-white">{userData.name}</div>
