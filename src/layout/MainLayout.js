6a26fb8554df34e0ccae3d059357c00ccf638ff1
Monogato <monogato.dg@gmail.com>
2022-09-30 20:04:20 -0500

    ADD Redirect if you dont logged

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,12 +1,24 @@
 import Header from '@components/Header';
 import Nav from '@common/Nav';
+import { useAuth } from '@hooks/useAuth';
+import { useRouter } from 'next/router';
 
 export default function MainLayout({ children }) {
+  const auth = useAuth();
+  const router = useRouter();
+
+  if (router.pathname !== '/' && router.pathname !== '/login' && !auth.user) {
+    setTimeout(() => {
+      console.log('No estas logeado');
+      router.push('/login/');
+    }, 0);
+  }
+
   return (
     <>
       <div className="min-h-full">
-        <Header />
-        <Nav />
+        {auth.user && <Header />}
+        {router.pathname !== '/' && <Nav />}
         <main>
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
         </main>
